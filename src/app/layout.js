eac1f24dc63343947b2515e100c8d1804c230898
khai <khaitruong1301@gmail.com>
2024-09-16 15:41:45 +0700

    dp

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,6 +30,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" /> 
+        <link rel="canonical" href="https://shoesshopbc70.vercel.app" />
       </head>
       <body className={`${roboto.className}`}>
         <HeaderHome />
